858553758bee818347fd7594c08970a169073189
tornadoblot <newclearplant@gmail.com>
2022-11-06 02:26:49 +0900

    26강 Templated Component

---
+++ BlazorApp/BlazorApp/Pages/FetchData.cs
@@ -0,0 +1,23 @@
+﻿//using BlazorApp.Data;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace BlazorApp.Pages
+//{
+//    public partial class FetchData
+//    {
+//        private WeatherForecast[] forecasts;
+
+//        protected override async Task OnInitializedAsync()
+//        {
+//            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+//        }
+//    }
+
+//    public partial class FetchData
+//    {
+
+//    }
+//}
--- BlazorApp/BlazorApp/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/TableTemplate.razor.g.cs
+++ BlazorApp/BlazorApp/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/TableTemplate.razor.g.cs
@@ -76,14 +76,13 @@ using BlazorApp.Shared;
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/electriclamb/Desktop/RookiesUnityPart6/BlazorApp/BlazorApp/Pages/FetchData.razor"
+#line 1 "/Users/electriclamb/Desktop/RookiesUnityPart6/BlazorApp/BlazorApp/Pages/TableTemplate.razor"
 using BlazorApp.Data;
 
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/fetchdata")]
-    public partial class FetchData : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class TableTemplate<TItem> : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
@@ -91,19 +90,21 @@ using BlazorApp.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 54 "/Users/electriclamb/Desktop/RookiesUnityPart6/BlazorApp/BlazorApp/Pages/FetchData.razor"
+#line 23 "/Users/electriclamb/Desktop/RookiesUnityPart6/BlazorApp/BlazorApp/Pages/TableTemplate.razor"
        
-    private WeatherForecast[] forecasts;
+    [Parameter]
+    public RenderFragment Header { get; set; }
 
-    protected override async Task OnInitializedAsync()
-    {
-        forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
-    }
+    [Parameter]
+    public RenderFragment<TItem> Row { get; set; }
+
+    [Parameter]
+    public IReadOnlyList<TItem> Items { get; set; }
+
 
 #line default
 #line hidden
 #nullable disable
-        [global::Microsoft.AspNetCore.Components.InjectAttribute] private WeatherForecastService ForecastService { get; set; }
     }
 }
 #pragma warning restore 1591
