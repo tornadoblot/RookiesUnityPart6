93cc77e7bac65b4882ad4d360b55f73121a3049b
tornadoblot <newclearplant@gmail.com>
2022-11-06 23:07:25 +0900

    30강 State 관리

---
+++ BlazorApp/BlazorApp2/Data/CounterState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace BlazorApp2.Data
+{
+    public class CounterState
+    {
+        int _count = 0;
+
+        public Action OnStateChanged;
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+
+            set
+            {
+                _count = value;
+                Refresh();
+            }
+        }
+
+        void Refresh()
+        {
+            OnStateChanged?.Invoke();
+        }
+    }
+}
