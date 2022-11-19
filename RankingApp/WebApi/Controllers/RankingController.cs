d2f7c1b3ec54ed51f5cc23d3555a3bb0a26ad709
tornadoblot <newclearplant@gmail.com>
2022-11-20 03:10:17 +0900

    38강 WebApi #3

--- RankingApp/WebApi/Controllers/RankingController.cs
+++ RankingApp/WebApi/Controllers/RankingController.cs
@@ -25,6 +25,14 @@ namespace WebApi.Controllers
         }
 
         // Create
+        [HttpPost]
+        public GameResult AddGameResult([FromBody] GameResult gameResult)
+        {
+            _context.GameResults.Add(gameResult);
+            _context.SaveChanges();
+
+            return gameResult; 
+        }
 
         // Read
         [HttpGet]
@@ -35,7 +43,6 @@ namespace WebApi.Controllers
                 .ToList();
 
             return results;
-                
         }
 
         [HttpGet("{id}")]
@@ -51,8 +58,39 @@ namespace WebApi.Controllers
 
 
         // Update
+        [HttpPut]
+        public bool UpdateGameResult([FromBody] GameResult gameResult)
+        {
+            var findResult = _context.GameResults
+                .Where(x => x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+                return false;
+
+            findResult.UserName = gameResult.UserName;
+            findResult.Score = gameResult.Score;
+            _context.SaveChanges();
+
+            return true;
+        }
 
         // Delete
+        [HttpDelete("{id}")]
+        public bool DeleteGameResult(int id)
+        {
+            var findResult = _context.GameResults
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+                return false;
+
+            _context.GameResults.Remove(findResult);
+            _context.SaveChanges();
+
+            return true;
+        }
 
     }
 }
