5c4a861daffcd67be6fede7b2f5089c6d0f20a26
tornadoblot <newclearplant@gmail.com>
2022-11-21 01:49:13 +0900

    40강 Unity와 WebApi 연동

---
+++ WebTest/Assets/WebManager.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameResult
+{
+    public string UserName;
+    public int Score;
+}
+
+public class WebManager : MonoBehaviour
+{
+    string _baseUrl = "https://localhost:5001/api";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameResult res = new GameResult()
+        {
+            UserName = "elamb",
+            Score = 999
+        };
+
+        SendPostRequest("ranking", res, (uwr) =>
+        {
+            Debug.Log("TODO: UI 갱신하기");
+        });
+
+        SendGetAllRequest("ranking", (uwr) =>
+        {
+            Debug.Log("TODO: UI 갱신하기");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SendPostRequest(string url, object obj, Action<UnityWebRequest> callback)
+    {
+        StartCoroutine(CoSendWebRequest(url, "POST", obj, callback));
+    }
+
+    public void SendGetAllRequest(string url, Action<UnityWebRequest> callback)
+    {
+        StartCoroutine(CoSendWebRequest(url, "GET", null, callback));
+    }
+
+
+    IEnumerator CoSendWebRequest(string url, string method, object obj, Action<UnityWebRequest> callback)
+    {
+        yield return null;
+
+        string sendUrl = $"{_baseUrl}/{url}";
+
+        byte[] jsonBytes = null;
+
+        if(obj != null)
+        {
+            string jsonStr = JsonUtility.ToJson(obj);
+            jsonBytes = Encoding.UTF8.GetBytes(jsonStr);
+        }
+
+        var uwr = new UnityWebRequest(sendUrl, method);
+        uwr.uploadHandler = new UploadHandlerRaw(jsonBytes);
+        uwr.downloadHandler = new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+
+        yield return uwr.SendWebRequest();
+
+        if(uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log(uwr.error);
+        }
+        else
+        {
+            Debug.Log("Recv " + uwr.downloadHandler.text);
+            callback.Invoke(uwr);
+        }
+
+    }
+}
